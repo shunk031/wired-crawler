78c7484bd2b1fdd4afac61d121f19eebeacbffc4
Shunsuke KITADA <septemtrio.ager@gmail.com>
2017-01-14 02:03:12 +0900

    add main scripts

---
+++ wired_crawler/crawler.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from wired_crawler.scraper import WiredScraper
+from bs4 import BeautifulSoup
+
+import time
+
+try:
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib2 import urlopen
+    from urllib2 import HTTPError
+
+
+class WiredCrawler:
+
+    page_count = 1
+    FINISH_CRAWL = "Finish crawl!"
+
+    def __init__(self, target_url, save_dir="./data"):
+        self.target_url = target_url
+        self.before_url = None
+        self.save_dir = save_dir
+
+    def _make_soup(self, url):
+        try:
+            with urlopen(url) as response:
+                html = response.read()
+
+            return BeautifulSoup(html, "lxml")
+
+        except HTTPError as e:
+            print("[ DEBUG ] in WiredCrawler#make_soup: {}".format(e))
+            return None
+
+    def get_next_page_link(self, url):
+
+        self.before_url = url
+        soup = self._make_soup(self.target_url)
+        div_pagination = soup.find("div", {"class": "pagination"})
+        a_next = div_pagination.find("a", {"class": "next"})
+
+        if a_next is not None and "href" in a_next.attrs:
+            next_page_url = a_next["href"]
+
+            if self.before_url != next_page_url:
+                print("[ DEBUG ] Next article list page: {}".format(url))
+                return next_page_url
+
+        return None
+
+    def crawl(self):
+
+        while True:
+            print("[ DEBUG ] Now page {} PROCESSING".format(self.page_count))
+            scraper = WiredScraper(self.target_url, self.save_dir)
+            scraper.scrap()
+            self.target_url = self.get_next_page_link(self.target_url)
+
+            if self.target_url is None:
+                break
+
+            self.page_count += 1
+            time.sleep(2)
+
+        return self.FINISH_CRAWL
---
+++ wired_crawler/scraper.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from bs4 import BeautifulSoup
+
+import os
+import csv
+
+try:
+    from urllib.request import urlopen
+    from urllib.error import HTTPError
+except ImportError:
+    from urllib2 import urlopen
+    from urllib2 import HTTPError
+
+
+class WiredScraper:
+
+    def __init__(self, target_url, save_dir):
+        self.target_url = target_url
+        self.save_dir = save_dir
+
+    def _make_soup(self, url):
+        try:
+            with urlopen(url) as response:
+                html = response.read()
+
+            return BeautifulSoup(html, "lxml")
+
+        except HTTPError as e:
+            print("[ DEBUG ] in WiredScraper#make_soup: {}".format(e))
+            return None
+
+    def scrap(self):
+
+        article_detail_url_list = self.get_article_detail_urls()
+
+        article_detail_info = []
+        for article_url in article_detail_url_list:
+            article_dict = self.get_article_detail_info_dict(article_url)
+            article_detail_info.append(article_dict)
+
+        self.save_article_detail_info_list(article_detail_info)
+
+    def get_article_detail_urls(self):
+
+        soup = self._make_soup(self.target_url)
+
+        # 記事概要一覧を取得する
+        ul_col = soup.find("ul", {"class": "col"})
+
+        # 記事概要のそれぞれのデータを取得する
+        li_articles = ul_col.find_all("li")
+
+        # 記事詳細へのURLを取得する
+        article_detail_url_list = []
+        for li_article in li_articles:
+            a_pad = li_article.find("a", {"class": "clearfix"})
+            url = a_pad["href"]
+            print("[ DEBUG ] Get URL: {}".format(url))
+            article_detail_url_list.append(url)
+
+        return article_detail_url_list
+
+    def get_article_detail_info_dict(self, article_url):
+        article_dict = {}
+        article_dict["url"] = article_url
+
+        detail_soup = self._make_soup(article_url)
+        h1_post_title = detail_soup.find("h1", {"class": "post-title"})
+        title = h1_post_title.get_text()
+        print("[ DEBUG ] Title: {}".format(title))
+        article_dict["title"] = title
+
+        article_content = detail_soup.find("article", {"class": "content"})
+        article_dict["article"] = article_content.get_text()
+
+        return article_dict
+
+    def save_article_detail_info_list(self, article_detail_info_list):
+
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+
+        for article_detail_dict in article_detail_info_list:
+            article_title = article_detail_dict["title"]
+            csv_filename = self._convert_filename(article_title)
+            csv_filename = "{}.csv".format(csv_filename)
+
+            with open(os.path.join(self.save_dir, csv_filename), "w") as wf:
+                writer = csv.writer(wf)
+                writer.writerow([article_detail_dict["title"],
+                                 article_detail_dict["url"],
+                                 article_detail_dict["article"]])
+
+    def _convert_filename(self, article_title):
+
+        filename = article_title.replace(" ", "_")
+        filename = filename.replace("/", "")
+        return filename

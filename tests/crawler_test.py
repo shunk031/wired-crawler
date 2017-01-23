b996dc5766ff6e9b8b9c072210d374929953d753
Shunsuke KITADA <septemtrio.ager@gmaul.com>
2017-01-23 22:12:23 +0900

    update test script

--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -12,23 +12,35 @@ HOME_DIR = os.path.expanduser("~")
 
 if __name__ == '__main__':
 
-    WIRED_URL = "https://www.wired.com/category/science/page/1"
-    crawler = WiredCrawler(WIRED_URL)
+    if os.path.isfile("status.json"):
+        with open("status.json", "r") as rf:
+            status_dict = json.load(rf)
+
+        WIRED_URL = status_dict["target_url"]
+        page_count = status_dict["page_count"]
+
+    else:
+        WIRED_URL = "https://www.wired.com/category/science/page/1"
+        page_count = 1
+
+    crawler = WiredCrawler(WIRED_URL, "./dump_files", page_count=page_count)
+
+    slacker_config = os.path.join(HOME_DIR, ".slacker.config")
+    with open(slacker_config, "r") as rf:
+        config = json.load(rf)
+
+    slacker = Slacker(config["token"])
 
     try:
-        crawler.crawl()
+        finish_crawl = crawler.crawl()
+        slacker.chat.post_message("#crawler", "{}: {}".format(crawler.__class__.__name__, finish_crawl))
+
     except Exception as e:
-
-        slacker_config = os.path.join(HOME_DIR, ".slacker.config")
-        with open(slacker_config, "r") as rf:
-            config = json.load(rf)
-
-        slacker = Slacker(config["token"])
 
         trial = 0
         while trial < 3:
             try:
-                slacker.chat.post_message("#crawler", e)
+                slacker.chat.post_message("#crawler", "{}: {}".format(crawler.__class__.__name__, e))
             except Exception:
                 trial += 1
             else:

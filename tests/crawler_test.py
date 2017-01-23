45a658df01eb1f084a1e2290e216d1fc2c7d1b6b
Shunsuke KITADA <septemtrio.ager@gmaul.com>
2017-01-23 18:09:12 +0900

    add slacker setting

--- tests/crawler_test.py
+++ tests/crawler_test.py
@@ -5,9 +5,31 @@ import os
 sys.path.append(os.pardir)
 
 from wired_crawler.crawler import WiredCrawler
+from slacker import Slacker
+import json
+
+HOME_DIR = os.path.expanduser("~")
 
 if __name__ == '__main__':
 
     WIRED_URL = "https://www.wired.com/category/science/page/1"
     crawler = WiredCrawler(WIRED_URL)
-    crawler.crawl()
+
+    try:
+        crawler.crawl()
+    except Exception as e:
+
+        slacker_config = os.path.join(HOME_DIR, ".slacker.config")
+        with open(slacker_config, "r") as rf:
+            config = json.load(rf)
+
+        slacker = Slacker(config["token"])
+
+        trial = 0
+        while trial < 3:
+            try:
+                slacker.chat.post_message("#crawler", e)
+            except Exception:
+                trial += 1
+            else:
+                break
